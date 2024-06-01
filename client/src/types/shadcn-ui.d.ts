8fc16d3c61fe383d23489dbf826df68391837292
coutinhomarco <marquinco@outlook.com>
2024-06-01 11:05:14 -0300

    feat: add shadcn

---
+++ client/src/types/shadcn-ui.d.ts
@@ -0,0 +1,10 @@
+declare module '@shadcn/ui' {
+    import * as React from 'react';
+  
+    export const Input: React.FC<React.InputHTMLAttributes<HTMLInputElement>>;
+    export const Button: React.FC<React.ButtonHTMLAttributes<HTMLButtonElement>>;
+    export const Card: React.FC<React.HTMLAttributes<HTMLDivElement>>;
+    export const CardHeader: React.FC<React.HTMLAttributes<HTMLDivElement>>;
+    export const CardBody: React.FC<React.HTMLAttributes<HTMLDivElement>>;
+  }
+  
