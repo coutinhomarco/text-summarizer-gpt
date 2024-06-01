de401e8121683d4196cd710b7889c16141192ce0
coutinhomarco <marquinco@outlook.com>
2024-06-01 11:37:57 -0300

    remove footer

--- client/src/layouts/layout.tsx
+++ client/src/layouts/layout.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import Header from '../components/header';
-import Footer from '../components/footer';
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="flex flex-col min-h-screen bg-gray-100 dark:bg-gray-900 text-gray-900 dark:text-gray-100">
       <Header />
       <main className="flex-grow">{children}</main>
-      <Footer />
     </div>
   );
 };
